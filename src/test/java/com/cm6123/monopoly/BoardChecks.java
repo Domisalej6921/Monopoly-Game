cd5a0d9bfebed55888143e8d4b91b771e3c9b78f
Domisalej6921 <dominickgeorge21@gmail.com>
2024-04-25 15:56:26 +0100

    Finalised Tests for the boardInput() method in Boards.java. However, haven't finished the boardCreation method so haven't written tests for it either.

--- src/test/java/com/cm6123/monopoly/BoardChecks.java
+++ src/test/java/com/cm6123/monopoly/BoardChecks.java
@@ -1,9 +1,13 @@
 package com.cm6123.monopoly;
 
 import com.cm6123.monopoly.game.Board;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import java.io.ByteArrayInputStream;
+import java.io.ByteArrayOutputStream;
+import java.io.InputStream;
 import java.util.Scanner;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
@@ -13,20 +17,43 @@ import static org.junit.jupiter.api.Assertions.assertTrue;
 public class BoardChecks {
 
     @Test
-    public void testBoardInput() {
-        String input = "25";
+    public void testValidInput() {
+        //Preparing input
+        String input = "30";
+        InputStream in = new ByteArrayInputStream(input.getBytes());
+        Scanner scanner = new Scanner(in);
 
-        System.setIn(new ByteArrayInputStream(input.getBytes()));
+        //Call the method
+        int result = Board.boardInput(scanner);
 
-        Scanner scanner = new Scanner(System.in);
+        //Validate result
+        assertEquals(30, result);
+    }
 
-        Board aBoard = new Board();
+    @Test
+    public void testInvalidInputThenValidInput() {
+        //Preparing input
+        String input = "5\n60\n30";
+        InputStream in = new ByteArrayInputStream(input.getBytes());
+        Scanner scanner = new Scanner(in);
 
-        int userInput = Board.boardInput(scanner);
+        //Calling method
+        int result = Board.boardInput(scanner);
 
-        assertTrue(userInput >= 10 && userInput <= 50);
+        //Validating result
+        assertEquals(30, result);
+    }
 
-        scanner.close();
+    @Test
+    public void testBoundaryInput() {
+        //Preparing input
+        String input = "10";
+        InputStream in = new ByteArrayInputStream(input.getBytes());
+        Scanner scanner = new Scanner(in);
+
+        //Calling method
+        int result = Board.boardInput(scanner);
+        assertEquals(10, result);
     }
 
     @Test
